f1886ac228c063d81a3bc517046d194d52b2ff15
nintien17 <lelinhtien102@gmail.com>
2023-05-25 15:03:26 +0700

    userlist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
-import { Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="app-container">
-   
           <Header/>
-          <TableUsers/>
+          <Container>
+            <TableUsers/>
+          </Container>
+          
     </div>
   );
 }
