5e31c7291ef2a73dd18a327b2a67e26a120f2c9b
nintien17 <lelinhtien102@gmail.com>
2023-05-26 19:36:30 +0700

    custom axios

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -13,8 +13,8 @@ const TableUsers = (props) => {
 
   const getUsers = async () => {
     let res = await fetchAllUser();
-    if (res && res.data && res.data.data) {
-      setlistUsers(res.data.data);
+    if (res && res.data) {
+      setlistUsers(res.data);
     }
   };
   console.log(listUsers);
